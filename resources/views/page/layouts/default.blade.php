4ba18223f70e494c2d1feb619cf8d3e868917739
Lucascodev <sopragames.sf@hotmail.com>
2022-11-21 14:53:32 -0300

    adding basic layout

---
+++ resources/views/page/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Super Gestão - @yield('title')</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('build/assets/css/style.css') }}">
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
